c0af0f6db29e0d4d6e67742c89ed15d1f7446ca2
whdcks3 <sj012944@gmail.com>
2024-02-21 17:15:42 +0900

    first

---
+++ Median.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Median {
+    public static void main(String[] args) {
+        int[] array=new int[]{1,2,7,10,11};
+        Arrays.sort(array);
+        int answer=array[array.length/2];
+        System.out.println(answer);
+    }
+}
+// https://school.programmers.co.kr/learn/courses/30/lessons/120811
