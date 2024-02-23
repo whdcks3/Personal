7f1ca0552528e7eb91452480bf055b5f18f4ffd0
whdcks3 <sj012944@gmail.com>
2024-02-23 17:11:59 +0900

    first

---
+++ num_pairs.java
@@ -0,0 +1,16 @@
+import java.util.stream.IntStream;
+
+public class num_pairs {
+    public static void main(String[] args) {
+        int n=20;
+        System.out.println(IntStream.rangeClosed(1, n).filter(i->n%i==0).count());
+        ////for문
+        // int answer=0;
+        // for(int i=1;i<=n;i++){
+        //     if (n%i==0){
+        //         answer++;
+        //     }
+        // }
+        // System.out.println(answer);
+    }
+}
