45570460666d9c6ea503e4711d776cc36b2467a0
whdcks3 <sj012944@gmail.com>
2024-02-27 23:12:24 +0900

    first

---
+++ Level0/Division.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Division {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n%2==0){
+            System.out.println(n+" is even");
+            sc.close();
+        }
+        else{
+            System.out.println(n+" is odd");
+            sc.close();
+        }
+    }
+}
