97425af0e74dd2bbe2127791b8acc9caa40c0d07
whdcks3 <sj012944@gmail.com>
2024-03-04 23:00:32 +0900

    first

---
+++ Level0/From_theback.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class From_theback {
+    public static void main(String[] args) {
+        int[] num_list = new int[] {12, 4, 15, 46, 38, 1, 14, 56, 32, 10};
+        int[] answer = new int[num_list.length-5];
+
+        Arrays.sort(num_list);
+        for(int i=5;i<num_list.length;i++){
+            answer[i-5]=num_list[i];
+        }
+        System.out.println(Arrays.toString(answer));
+    }
+}
+// https://school.programmers.co.kr/learn/courses/30/lessons/181852
--- Level2/Process.java
+++ Level2/Process.java
@@ -1,24 +1,38 @@
 package Level2;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 
 public class Process {
     public static void main(String[] args) {
         int[] Priorities = new int[] {2, 1, 3, 2};
         int location = 2;
 
-        Queue<Integer> q = new LinkedList<Integer>();
+        PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+
+        int ret = 0;
+            // 우선순위 큐에 우선순위 요소 추가
             for (int i : Priorities){
                 q.offer(i);    
             }
-            while (!q.isEmpty()) {
+
+            // 큐가 빌 때까지 반복
+            while (!q.isEmpty()) { // [3, 2, 2, 1]
                 for (int i=0; i<Priorities.length;i++){
+                    // 현재 위치에 작업에 대한 위치 확인
                     if (q.peek() == Priorities[i]){
                         q.poll();
+                        ret++;
+                        
+
+                        // 현재 작업이 location과 같으면 ret 반환
+                        if (location == i){
+                            System.out.println(ret);
+                        }
                     }
                 }
             }
-        System.out.println(q);
     }
 }
